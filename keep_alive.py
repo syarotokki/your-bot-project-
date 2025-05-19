8ccc1abf43bd13aaf6327405b79bbe13cb275336
syarotokki <maikuratokki@gmail.com>
2025-05-19 16:01:13 +0900

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot is alive!"
 
-def run():
+def keep_alive():
     app.run(host="0.0.0.0", port=8080)
